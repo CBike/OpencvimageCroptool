52d3168997bfc2d7295dc6f17dd22fc0fdf39dcd
yangho.jeon <64768242+CBike@users.noreply.github.com>
2022-06-10 10:15:44 +0900

    commit

--- Test.py
+++ Test.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import cv2
-from datetime import datetime
 from function import *
 
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
